eb573e5f38abd51d7f414213e0a48736a913249d
Hadiyan Kundrat Putra <hadiyankundrat@gmail.com>
2022-03-07 10:05:31 +0700

    [Initial Commit] Test PT.Saham Rakyat

---
+++ src/main/java/com/example/test/Nomor2.java
@@ -0,0 +1,42 @@
+package com.example.test;
+
+
+import java.util.Scanner;
+
+public class Nomor2 {
+    public static void main(String[] args) {
+        String kalimat;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Masukan Inputan ");
+        kalimat = keyboard.nextLine();
+        if (kalimat.isEmpty()) {
+            System.out.println("Masukan Kosong");
+        } else {
+            String poli = "";
+            for (int i = kalimat.length(); i > 0; i--) {
+                String test1 = kalimat.substring(i - 1, i);
+                poli = poli + test1;
+            }
+            if (kalimat.equals(poli)) {
+                System.out.println("Text Polindrome");
+            } else {
+                System.out.println("Text bukan Polindrome");
+            }
+        }
+    }
+
+//    private void palindrome(String teks) {
+//        String poli = "";
+//
+//        for (int i = teks.length(); i > 0; i--) {
+//            String test1 = teks.substring(i - 1, i);
+//            poli = poli + test1;
+//        }
+//
+//        if (teks.equals(poli)) {
+//            System.out.println("==== " + teks + " adalah Palindrome =====");
+//        } else {
+//            System.out.println("==== " + teks + " bukan Palindrome =====");
+//        }
+//    }
+}
